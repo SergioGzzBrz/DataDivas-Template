08c9eb6c14ce8f17001be7f6d1e2bbb77b8d7bfd
Sergio Gonzalez Barraza <114702763+SergioGzzBrz@users.noreply.github.com>
2026-07-29 12:39:44 -0600

    Add clock measurements to template

---
+++ content/contest/Clock.cpp
@@ -0,0 +1,5 @@
+#include <chrono>
+using namespace std::chrono;
+auto start = high_resolution_clock::now();
+auto stop = high_resolution_clock::now();
+auto duration = duration_cast<milliseconds>(stop - start);

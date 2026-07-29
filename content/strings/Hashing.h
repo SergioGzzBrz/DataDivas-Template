8e4148460d46c02ba9988a448aa08296a75f96d0
Sergio Gonzalez Barraza <114702763+SergioGzzBrz@users.noreply.github.com>
2026-07-29 12:20:14 -0600

    Add my string hashing

--- content/strings/Hashing.h
+++ content/strings/Hashing.h
@@ -1,54 +1,49 @@
+
 /**
  * Author: Simon Lindholm
  * Date: 2015-03-15
  * License: CC0
  * Source: own work
- * Description: Self-explanatory methods for string hashing.
+ * Description:\\
+\begin{minipage}{75mm}
+Self-explanatory methods for string hashing. 
+When comparing $n^2$ strings, change: 
+--- ll -> \_\_int128  
+--- static const \_\_int128 M = (\_\_int128(1) << 61) - 1;
+\end{minipage}
+\begin{minipage}{15mm}
+\includegraphics[width=\textwidth]{content/strings/HashingScenarios}
+\end{minipage}
  * Status: stress-tested
  */
 #pragma once
 
-// Arithmetic mod 2^64-1. 2x slower than mod 2^64 and more
-// code, but works on evil test data (e.g. Thue-Morse, where
-// ABBA... and BAAB... of length 2^10 hash the same mod 2^64).
-// "typedef ull H;" instead if you think test data is random,
-// or work mod 10^9+7 if the Birthday paradox is not a problem.
-typedef uint64_t ull;
-struct H {
-	ull x; H(ull x=0) : x(x) {}
-	H operator+(H o) { return x + o.x + (x + o.x < x); }
-	H operator-(H o) { return *this + ~o.x; }
-	H operator*(H o) { auto m = (__uint128_t)x * o.x;
-		return H((ull)m) + (ull)(m >> 64); }
-	ull get() const { return x + !~x; }
-	bool operator==(H o) const { return get() == o.get(); }
-	bool operator<(H o) const { return get() < o.get(); }
-};
-static const H C = (ll)1e11+3; // (order ~ 3e9; random also ok)
+struct HashedString {
+  static const ll M = 1e9 + 9;
+  static ll B;
+  // pow[i] contains B^i % M
+  static vector<ll> pow;
+  // p_hash[i] is the hash of the first i characters of the given string
+  vector<ll> p_hash;
 
-struct HashInterval {
-	vector<H> ha, pw;
-	HashInterval(string& str) : ha(sz(str)+1), pw(ha) {
-		pw[0] = 1;
-		rep(i,0,sz(str))
-			ha[i+1] = ha[i] * C + str[i],
-			pw[i+1] = pw[i] * C;
-	}
-	H hashInterval(int a, int b) { // hash [a, b)
-		return ha[b] - ha[a] * pw[b - a];
-	}
+  // can be changed to vi
+  HashedString(const string& s) : p_hash(s.size() + 1) {
+    while (pow.size() <= s.size()) {
+      pow.push_back((pow.back() * B) % M);
+    }
+
+    p_hash[0] = 0;
+    for (int i = 0; i < s.size(); i++) {
+      p_hash[i + 1] = ((p_hash[i] * B) % M + s[i]) % M;
+    }
+  }
+
+  // r inclusive, 0 index
+  ll get_hash(int start, int end) {
+    ll raw_val = (p_hash[end + 1] - (p_hash[start] * pow[end - start + 1]));
+    return (raw_val % M + M) % M;
+  }
 };
 
-vector<H> getHashes(string& str, int length) {
-	if (sz(str) < length) return {};
-	H h = 0, pw = 1;
-	rep(i,0,length)
-		h = h * C + str[i], pw = pw * C;
-	vector<H> ret = {h};
-	rep(i,length,sz(str)) {
-		ret.push_back(h = h * C + str[i] - pw * str[i-length]);
-	}
-	return ret;
-}
-
-H hashString(string& s){H h{}; for(char c:s) h=h*C+c;return h;}
+ll HashedString::B = rng(HashedString::M - 1);
+vi HashedString::pow = {1};
